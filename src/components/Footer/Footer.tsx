47adb5fb13027682ee16fab3bb9cb150e83f0cc4
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-23 11:59:38 +0200

    added footer and styling for nav

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.less";
+import Navbar from "../Header/Navbar/Navbar";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <Navbar />
+    </footer>
+  );
+}
