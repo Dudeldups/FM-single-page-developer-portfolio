0e7da5f61d8f3dc205d06215104c42a3bd0ed03f
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-23 19:36:11 +0200

    fixed max-width and header image

--- src/components/Header/Introduction/Introduction.tsx
+++ src/components/Header/Introduction/Introduction.tsx
@@ -3,14 +3,17 @@ import "./Introduction.less";
 export default function Introduction() {
   return (
     <section className="introduction">
-      <img
-        src="/assets/images/image-profile-mobile.webp"
-        alt="Adam Keyes"
-        className="introduction__img"
-      />
+      <aside>
+        <img
+          src="/assets/images/image-profile-mobile.webp"
+          alt="Adam Keyes"
+          className="introduction__img"
+        />
+      </aside>
       <div className="introduction__text">
         <h1 className="introduction__title">
-          Nice to meet you! I’m <span className="underline">Adam Keyes</span>.
+          <span>Nice to</span> meet you! I’m{" "}
+          <span className="underline">Adam Keyes</span>.
         </h1>
         <p className="introduction__desc">
           Based in the UK, I’m a front-end developer passionate about building
