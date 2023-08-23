1ce37509c2ec061d7c542ebb93a9467455850c86
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-23 14:24:15 +0200

    added json for skill display

--- src/components/MainContent/Skills/Skills.tsx
+++ src/components/MainContent/Skills/Skills.tsx
@@ -1,35 +1,25 @@
 import "./Skills.less";
+import skillsData from "../../../data/skillsData.json";
 
 export default function Skills() {
+  const allSkills = skillsData.map(skill => {
+    const { name, time } = skill;
+    return (
+      <li key={name} className="skills__item">
+        <h3 className="skills__title">{name}</h3>
+        <p className="skills__exp">{time} Years Experience</p>
+      </li>
+    );
+  });
+
   return (
-    <section className="skills">
-      <h2 className="sr-only">My skills</h2>
-      <ul className="skills__list">
-        <li className="skills__item">
-          <h3 className="skills__title">HTML</h3>
-          <p className="skills__exp">4 Years Experience</p>
-        </li>
-        <li className="skills__item">
-          <h3 className="skills__title">CSS</h3>
-          <p className="skills__exp">4 Years Experience</p>
-        </li>
-        <li className="skills__item">
-          <h3 className="skills__title">Javascript</h3>
-          <p className="skills__exp">4 Years Experience</p>
-        </li>
-        <li className="skills__item">
-          <h3 className="skills__title">Accessibility</h3>
-          <p className="skills__exp">4 Years Experience</p>
-        </li>
-        <li className="skills__item">
-          <h3 className="skills__title">React</h3>
-          <p className="skills__exp">3 Years Experience</p>
-        </li>
-        <li className="skills__item">
-          <h3 className="skills__title">Sass</h3>
-          <p className="skills__exp">3 Years Experience</p>
-        </li>
-      </ul>
-    </section>
+    <>
+      <hr />
+      <section className="skills">
+        <h2 className="sr-only">My skills</h2>
+        <ul className="skills__list">{allSkills}</ul>
+      </section>
+      <hr />
+    </>
   );
 }
